8fc46446747932266f60cc627971385a7402c3d7
Adeleke, Adetokunbo <Adetokunbo.Adeleke@ehi.com>
2019-09-11 13:18:37 +0100

    Add project files.

---
+++ quiz_angular_app/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("quiz_angular_app")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EHI")]
+[assembly: AssemblyProduct("quiz_angular_app")]
+[assembly: AssemblyCopyright("Copyright © EHI 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5d2a86d1-1a9e-4f89-aea9-394a6823ee61")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ quiz_angular_app/app/app.js
@@ -0,0 +1,38 @@
+﻿'use strict';
+
+var app = angular.module('quizApp', ['ngRoute', 'ngSanitize'])
+// Routing has been added to keep flexibility in mind. This will be used in future.
+angular.module('quizApp')
+.config(['$routeProvider',
+  function ($routeProvider) {
+
+      var routes = [
+          {
+              url: '/home',
+              template: 'templates/quiz.html', //the main entry point 
+              controller: 'quizCtrl'
+          },
+          //others not really useful
+          {
+              url: '/review',
+              template: 'templates/review.html', 
+              controller: 'reviewCtrl'
+          },
+          {
+              url: '/result',
+              template: 'templates/result.html',
+              controller: 'resultCtrl'
+          },
+          {
+              url: '/create',
+              template: 'templates/create.html',
+              controller: 'createCtrl'
+          }
+      ];
+
+      routes.forEach(function (r, index) {
+          $routeProvider.when(r.url, { templateUrl: r.template, controller: r.controller });
+      });
+
+      $routeProvider.otherwise({ redirectTo: '/home' });
+  }]);
